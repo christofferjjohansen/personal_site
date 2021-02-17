744ef17d86498e12643e9fe34222f866a83ff821
Christoffer Johansen <christoffer.jjohansen@gmail.com>
2021-02-16 20:59:32 -0800

    Change theme vars - just playing around atm

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - christoffer_johansen',
+    titleTemplate: '%s',
     title: 'christoffer_johansen',
     htmlAttrs: {
       lang: 'en'
@@ -50,8 +50,16 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
+        light: {
+          primary: '##030BA6',
+          accent: '#EBEFF2',
+          secondary: '##0476D9',
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        },
         dark: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
